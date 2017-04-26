387504c7cdfd7fbcf8bffea520136231f6aa550b
OxsanaP <oksanka-vredina@yandex.ru>
2017-04-26 23:39:35 +0300

    done page articles for aad articles

--- src/AppBundle/Form/ArticlesType.php
+++ src/AppBundle/Form/ArticlesType.php
@@ -3,11 +3,14 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class ArticlesType extends AbstractType
 {
@@ -17,8 +20,16 @@ class ArticlesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('title')
-            ->add('slug')
-            ->add('content')
+
+            ->add('slug', TextType::class, array(
+                'required'    => false,
+                'label' => 'Permanent link',
+            ))
+            ->add('content', TextareaType::class, array(
+                'attr' => array(
+                    'class' => 'tinymce',
+                    'data-theme' => 'bbcode' // Skip it if you want to use default theme
+                )))
             ->add('status', ChoiceType::class, array(
                 'choices' => array(
                     'Published' => 'Published',
